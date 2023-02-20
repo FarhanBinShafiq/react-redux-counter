6ee088b22bf5ab752706c78be72ee5f058095bee
FarhanBinShafiq <farhanbinshafiq@gmail.com>
2023-02-20 20:03:11 +0600

    react redux counter

--- src/App.js
+++ src/App.js
@@ -1,75 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import State from './components/State';
-import { useState } from 'react';
+ import React from 'react';
 import Counter from './components/Counter';
-
-const initialState = [
-
-  {
-    id: 1,
-    count: 0
-  },
-  {
-    id: 2,
-    count: 0
-  }
-
-]
-
-function App() {
-  const [state, setState] = useState(initialState)
-
-  const totalCount = () => {
-    return state.reduce((total, counter) => total + counter.count, 0)
-  }
-
-  const increment = (id) => {
-    const updatedCounter = state.map(c => {
-      if (c.id === id) {
-        return {
-          ...c,
-          count: c.count + 1
-        }
-      }
-      return { ...c }
-    })
-    setState(updatedCounter)
-  }
-
-  const decrement = (id) => {
-    const updatedCounter = state.map(c => {
-      if (c.id === id) {
-        return {
-          ...c,
-          count: c.count - 1
-        }
-      }
-      return {...c}
-    })
-    setState(updatedCounter)
-  }
-
+ 
+ const App = () => {
   return (
-    <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-    <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
-        Simple Counter Application
-    </h1>
+    <div>
+      <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+ 
+            <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+                Simple Counter Application
+            </h1>
 
-    <div class="max-w-md mx-auto mt-10 space-y-5">
-        {state.map((count) => (
-            <Counter
-                key={count.id}
-                id={count.id}
-                count={count.count}
-                increment={increment}
-                decrement={decrement}
-            />
-        ))}
-        <State count={totalCount()} />
+           
+           <>
+             <Counter id={2}/>
+             <Counter id={3}/>
+           </>
+        </div>
+
     </div>
-</div>
   );
-}
-
-export default App;
+ };
+ 
+ export default App;
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,48 @@
 import React from 'react';
-import Button from './Button';
-import Count from './Count';
+import { connect } from 'react-redux';
+import { decrement, increment } from '../redux/counter/actions';
 
-const Counter = ({id,count,increment,decrement}) => {
+const Counter = ({count, increment, decrement}) => {
     return (
-        <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-        <Count count={count} />
-        <div class="flex space-x-3">
-            <Button handler={()=>increment(id)}>Incremnt</Button>
-            <Button handler={()=>decrement(id)}>Decrement</Button>
+        <div>
+            <div class="max-w-md mx-auto mt-10 space-y-5">
+                <div
+                    class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
+                >
+                    <div class="text-2xl font-semibold">{count}</div>
+                    <div class="flex space-x-3">
+                        <button
+                            class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+                            onClick={increment}
+                        >
+                            Increment
+                        </button>
+                        <button
+                            class="bg-red-400 text-white px-3 py-2 rounded shadow"
+                            onClick={decrement}
+                        >
+                            Decrement
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
     );
 };
 
-export default Counter;
+const mapStateToProps = (state,ownProps) => {
+    console.log(ownProps)
+    return {
+        
+        count: state.value
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        increment: (value) => dispatch(increment(value)),
+        decrement: (value) => dispatch(decrement(value))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
